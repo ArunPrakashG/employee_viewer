4d6cee690a1c4c4ec176f83ab4460c4544d9f126
Arun Prakash <arun.prakash.456789@gmail.com>
2021-09-18 10:18:09 +0530

    ui pending

---
+++ lib/employee_viewer.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'ui/screens/home/home.dart';
+
+class EmployeeViewerApp extends StatelessWidget {
+  const EmployeeViewerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Employee Viewer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeUI(),
+    );
+  }
+}
